2054a6eadbec54d39984a3bc65c8caa8861c3e0d
LilacTree <50721867+LilacTree@users.noreply.github.com>
2019-05-27 20:14:46 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# whiskers-in-combat
-Receive message when you enter combat while Angler Whiskers are equipped.
+# whiskers-alert
+TERA-proxy module for TERA Online that alerts you when you enter combat while Angler's Whiskers are equipped, as well as when you use bait while Angler's Whiskers are not equipped.
 
 ## Config
-```
-proxyMessage [true/false] - Get a message on proxy channel /8
-whisperMessage [true/false] - Get a message as a whisper
-```
+- `proxyMessage` [true/false] - Get a message on proxy channel /8
+- `whisperMessage` [true/false] - Get a message as a whisper
+
+## Notes
+- Modify the config file to customize settings
--- index.js
+++ index.js
@@ -3,8 +3,10 @@ String.prototype.clr = function (hexColor) { return `<font color='#${hexColor}'>
 
 module.exports = function WhiskersInCombat(mod) {    
     const WhiskerIDs = [206100, 206101, 206102, 206103, 206104, 206105, 206106, 206107, 206108, 206109];
-    const Message = 'Angler Whiskers are equipped!';
-    
+    const Message = 'Angler Whisker is equipped!';
+	const BaitIDs = [206001, 206002, 206003, 206004, 206006, 206007, 206008, 206009];
+	const Message2 = 'Angler Whisker is NOT equipped!';
+	
     mod.game.initialize('inventory');
     
     mod.game.me.on('enter_combat', () => { 
@@ -20,5 +22,20 @@ module.exports = function WhiskersInCombat(mod) {
                 });
         }
     })
+	
+	mod.hook('C_USE_ITEM', 3, (event) => {
+		if (!mod.game.inventory.findInEquipment(WhiskerIDs) && BaitIDs.includes(event.id)) {
+			
+			if (mod.settings.proxyMessage) 
+                mod.command.message(Message2.clr("FF0000"));
+            
+            if (mod.settings.whisperMessage)
+                mod.send('S_CHAT', 2, {
+                    channel: 7, 
+                    authorName: '',
+                    message: Message2
+                });
+		}
+	});
 
 }
