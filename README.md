16dda27b618c32311cde09cee5c5b22d66bd5510
LilacTree <50721867+LilacTree@users.noreply.github.com>
2019-06-20 21:26:07 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # whiskers-alert
-TERA-proxy module for TERA Online that alerts you when you enter combat while Angler's Whiskers are equipped, as well as when you use bait while Angler's Whiskers are not equipped.
+TERA-proxy module for TERA Online that alerts you when you enter combat while Angler's Whiskers are equipped, as well as when you use bait/rod while Angler's Whiskers are not equipped.
 
 ## Config
 - `proxyMessage` [true/false] - Get a message on proxy channel /8
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function WhiskersAlert(mod) {
     const WhiskerIDs = [206100, 206101, 206102, 206103, 206104, 206105, 206106, 206107, 206108, 206109];
     const Message = 'Angler Whisker is equipped!';
 	const BaitIDs = [206001, 206002, 206003, 206004, 206006, 206007, 206008, 206009];
+	const ROD_IDs = [206700, 206701, 206702, 206703, 206704, 206705, 206706, 206707, 206708, 206709, 206710, 206711, 206712, 206713, 206714, 206715, 206716, 206717, 206718, 206719, 206720, 206721, 206722, 206723, 206724, 206725, 206726, 206727, 206728];
 	const Message2 = 'Angler Whisker is NOT equipped!';
 	
     mod.game.initialize('inventory');
@@ -24,7 +25,7 @@ module.exports = function WhiskersAlert(mod) {
     })
 	
 	mod.hook('C_USE_ITEM', 3, (event) => {
-		if (!mod.game.inventory.findInEquipment(WhiskerIDs) && BaitIDs.includes(event.id)) {
+		if (!mod.game.inventory.findInEquipment(WhiskerIDs) && (BaitIDs.includes(event.id) || ROD_IDs.includes(event.id))) {
 			
 			if (mod.settings.proxyMessage) 
                 mod.command.message(Message2.clr("FF0000"));
